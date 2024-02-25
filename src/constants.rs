de32e40bee5d959cb4114b79033cb4840230c9ff
Kenneth Love <kennethlove@gmail.com>
2024-02-25 09:39:43 -0800

    better sprite sizes and constant names

--- src/constants.rs
+++ src/constants.rs
@@ -3,14 +3,14 @@ pub const DRAW_GIZMOS: bool = true;
 pub const WIDTH: f32 = 600.;
 pub const HEIGHT: f32 = 400.;
 
-pub const SPRITE_WIDTH: u32 = 32;
+pub const SPRITE_WIDTH: u32 = 16;
 pub const SPRITE_HEIGHT: u32 = 32;
 
 pub const SAFE_BUFFER: f32 = SPRITE_WIDTH as f32 * 1.75;
 pub const SAFE_WIDTH: f32 = WIDTH as f32 - SAFE_BUFFER;
 pub const SAFE_HEIGHT: f32 = HEIGHT as f32 - SAFE_BUFFER;
 
-pub const KNIGHT_SPEED: f32 = 200.;
-pub const KNIGHT_SPEED_FAST: f32 = 300.;
+pub const PAWN_SPEED: f32 = 200.;
+pub const PAWN_SPEED_FAST: f32 = 300.;
 
 pub const GOBLIN_SPEED: f32 = 0.1;
