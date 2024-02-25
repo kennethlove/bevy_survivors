d7f74f689e99fa444c122cc2f6e4577b82d2e307
Kenneth Love <kennethlove@gmail.com>
2024-02-25 09:58:26 -0800

    current pawns are 16x16

--- src/constants.rs
+++ src/constants.rs
@@ -4,7 +4,7 @@ pub const WIDTH: f32 = 600.;
 pub const HEIGHT: f32 = 400.;
 
 pub const SPRITE_WIDTH: u32 = 16;
-pub const SPRITE_HEIGHT: u32 = 32;
+pub const SPRITE_HEIGHT: u32 = 16;
 
 pub const SAFE_BUFFER: f32 = SPRITE_WIDTH as f32 * 1.75;
 pub const SAFE_WIDTH: f32 = WIDTH as f32 - SAFE_BUFFER;
